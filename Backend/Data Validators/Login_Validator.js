c2113c49cbade9db383d1ed25ada5fe1a369cc56
NaveenKumarReddyK <naveen623.nr@gmail.com>
2020-11-06 18:08:13 +0530

    Validators added

---
+++ Backend/Data Validators/Login_Validator.js
@@ -0,0 +1,34 @@
+// const validateFunc = require('validator')
+import validateFunc from "validator";
+
+module.exports = function isLoginDataValid(login_data) {
+  //initialize errors
+  const login_Errors = {};
+
+  var userEmail = !validateFunc.isEmpty(login_data.user_email)
+    ? login_data.user_email
+    : "";
+  var userPwd = !validateFunc.isEmpty(login_data.user_pwd)
+    ? login_data.user_pwd
+    : "";
+
+  //check if email is of correct format
+  if (!validateFunc.isEmpty(userEmail)) {
+    login_Errors.emailError = "Email Should Not Be Empty";
+  } else if (!validateFunc.isEmail(userEmail)) {
+    login_Errors.emailError = "Please Correct Email Address";
+  }
+
+  //check for password length
+  if (!validateFunc.isEmpty(userPwd)) {
+    login_Errors.pwdError = "Password Should Not Be Empty";
+  } else if (!validateFunc.isLenght(userPwd, 0, 8)) {
+    login_Errors.pwdError = "Password Length Should Be In Between 0 and 8";
+  }
+
+  //return all the errors
+  return {
+    isValid: !validateFunc.isEmpty(login_Errors),
+    login_Errors,
+  };
+};
---
+++ Backend/Data Validators/Registeration_Validator.js
@@ -0,0 +1,43 @@
+// const validateFunc = require('validator')
+import validateFunc from "validator";
+
+module.exports = function isRegDataValid(reg_data) {
+  //initialize errors
+  const reg_Errors = {};
+
+  //check if inputs are empty
+  var userName = !validateFunc.isEmpty(reg_data.user_name)
+    ? reg_data.user_name
+    : "";
+  var userEmail = !validateFunc.isEmpty(reg_data.user_email)
+    ? reg_data.user_email
+    : "";
+  var userPwd = !validateFunc.isEmpty(reg_data.user_pwd)
+    ? reg_data.user_pwd
+    : "";
+
+  //check if username is empty
+  if (!validateFunc.isEmpty(userName)) {
+    reg_Errors.uNameError = "User Name Should Not Be Empty";
+  }
+
+  //check if email is of correct format
+  if (!validateFunc.isEmpty(userEmail)) {
+    reg_Errors.emailError = "Email Should Not Be Empty";
+  } else if (!validateFunc.isEmail(userEmail)) {
+    reg_Errors.emailError = "Please Correct Email Address";
+  }
+
+  //check for password length
+  if (!validateFunc.isEmpty(userPwd)) {
+    reg_Errors.pwdError = "Password Should Not Be Empty";
+  } else if (!validateFunc.isLenght(userPwd, 0, 8)) {
+    reg_Errors.pwdError = "Password Length Should Be In Between 0 and 8";
+  }
+
+  //return all the errors
+  return {
+    isValid: !validateFunc.isEmpty(reg_Errors),
+    reg_Errors,
+  };
+};
