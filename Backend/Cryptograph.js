033e795cae76ab4b0fcee91641fef85aaa68ac26
NaveenKumarReddyK <naveen623.nr@gmail.com>
2020-11-06 14:57:44 +0530

    encryption and decryption functions added

--- Backend/Cryptograph.js
+++ Backend/Cryptograph.js
@@ -1,31 +1,66 @@
 const crypto = require("crypto-js");
+
+//####################  ECCRYPTION ########################
+//function to encrypt password
+module.exports = function encryptStoredPassword(curr_pwd,master_pwd){
+
+//key is the SHA256 hashed master password of user
 //key is the sha512 hash of user password
-var key = "pbuYKcaZgo+nq/dWNW37usBM6lqQNGQ1KGA2yVP3xl8=";
+//SHA256 encryption of user password
+
+var shaEnc = crypto.SHA256(master_pwd);
+// console.log("SHA 256", shaEnc.toString(crypto.enc.Base64));
+var key = shaEnc.toString(crypto.enc.Base64);
 var keyHex = crypto.enc.Utf8.parse(key);
 
-console.log("keyhex", crypto.enc.Base64.parse(key).toString());
-var message = "naveen";
+// console.log("keyhex", crypto.enc.Base64.parse(key).toString());
+
+var message = curr_pwd;
+//encryption using triple DES in ECB mode
 var encrypted = crypto.TripleDES.encrypt(message, keyHex, {
   mode: crypto.mode.ECB,
   padding: crypto.pad.Pkcs7,
 });
-
 var encStr = encrypted.toString();
-console.log("encStr", encrypted.ciphertext.toString());
 
-var decrytped = crypto.TripleDES.decrypt(
-  {
-    ciphertext: crypto.enc.Base64.parse(encStr),
-  },
-  keyHex,
-  {
-    mode: crypto.mode.ECB,
-    padding: crypto.pad.Pkcs7,
-  }
-);
+// console.log("encStr", encrypted.ciphertext.toString());
 
-console.log("Decrytped", decrytped.toString(crypto.enc.Utf8));
+return {
+    enc_pwd : encStr,
+    enc_pwd_String : encrypted.ciphertext.toString()
+}
 
-//SHA256 encryption of user password
-var shaEnc = crypto.SHA256("naveen");
-console.log("SHA 256", shaEnc.toString(crypto.enc.Base64));
+}
+
+
+//####################  DECRYPTION ########################
+//function to decrypt password
+module.exports = function decryptStoredPassword(enc_pwd,master_pwd){
+    //SHA256 algo encrypt to master password to use it as key
+
+    var shaEnc = crypto.SHA256(master_pwd);
+    // console.log("SHA 256", shaEnc.toString(crypto.enc.Base64));
+    var key = shaEnc.toString(crypto.enc.Base64);
+    var keyHex = crypto.enc.Utf8.parse(key);
+
+    var decrytped = crypto.TripleDES.decrypt(
+        {
+          ciphertext: crypto.enc.Base64.parse(enc_pwd),
+        },
+        keyHex,
+        {
+          mode: crypto.mode.ECB,
+          padding: crypto.pad.Pkcs7,
+        }
+      );
+      
+    //   console.log("Decrytped", decrytped.toString(crypto.enc.Utf8));
+    return{
+        decrpyt_pwd : decrytped.toString(crypto.enc.Utf8) //to be displayed in string format
+    }
+}
+
+
+
+
+
