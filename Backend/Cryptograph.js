8d22cc27431627c5db8f2521fbf40f614d054184
NaveenKumarReddyK <naveen623.nr@gmail.com>
2020-11-06 14:07:22 +0530

    crypto file updates

--- Backend/Cryptograph.js
+++ Backend/Cryptograph.js
@@ -1,3 +1,31 @@
-const crypto = require('crypto-js')
-var hash = crypto.MD5("naveen").toString()
-console.log(hash)
+const crypto = require("crypto-js");
+//key is the sha512 hash of user password
+var key = "pbuYKcaZgo+nq/dWNW37usBM6lqQNGQ1KGA2yVP3xl8=";
+var keyHex = crypto.enc.Utf8.parse(key);
+
+console.log("keyhex", crypto.enc.Base64.parse(key).toString());
+var message = "naveen";
+var encrypted = crypto.TripleDES.encrypt(message, keyHex, {
+  mode: crypto.mode.ECB,
+  padding: crypto.pad.Pkcs7,
+});
+
+var encStr = encrypted.toString();
+console.log("encStr", encrypted.ciphertext.toString());
+
+var decrytped = crypto.TripleDES.decrypt(
+  {
+    ciphertext: crypto.enc.Base64.parse(encStr),
+  },
+  keyHex,
+  {
+    mode: crypto.mode.ECB,
+    padding: crypto.pad.Pkcs7,
+  }
+);
+
+console.log("Decrytped", decrytped.toString(crypto.enc.Utf8));
+
+//SHA256 encryption of user password
+var shaEnc = crypto.SHA256("naveen");
+console.log("SHA 256", shaEnc.toString(crypto.enc.Base64));
